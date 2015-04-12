312d13ff07a0c771a5ac04f4cdb54ed6aca9a6c1
Andrii Vasyliev <sol@hiqdev.com>
2015-04-12 09:00:12 +0000

    inited

---
+++ Migration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link    http://hiqdev.com/yii2-hidb
+ * @license http://hiqdev.com/yii2-hidb/license
+ * @copyright Copyright (c) 2015 HiQDev
+ */
+
+namespace hiqdev\hidb;
+
+class Migration extends \yii\db\Migration
+{
+    public function createTable ($table, $columns, $options = []) {
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Yii2 advanced DB
+----------------
+
+- Cross-DBMS queries and migrations
+- more...
+
+For more info see wiki:
+https://github.com/hiqdev/yii2-hidb/wiki
